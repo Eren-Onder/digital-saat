2b8a5d5448f0975d6af1e92aa15767bfce19c6a8
E2395-Eren <2395-Eren@users.noreply.github.com>
2021-01-28 20:12:59 +0100

    digital saat

---
+++ app.js
@@ -0,0 +1,19 @@
+/**
+ * Dünya genelinde 3 şehrin saatini gösteren program.
+ * Her şehre Tıklandığında şehrin saati doğru olarak gösterilmeli.
+ * Ekranda çalışan saatin hangi şehire ait olduğu gösterilmeli.
+ * saat aktif çalışıyor olmalı, setinterval kullanarak her saniye yenilenebilir.
+ * gösterilmek istenen şehir seçildiğinde diğer iki şehrin yenilenmesi clearInterval ile durulmalı.
+ * saat - dakika - saniye değerleri 10 dan küçük olduğunda yanlarına "0" sıfır eklenmeli.
+ *
+ */
+
+let baslik = document.querySelector("h1");
+let neresiTokyo = document.querySelector("#tokyo");
+let neresiNewYork = document.querySelector("#newYork");
+let neresiZurich = document.querySelector("#zurich");
+let sehirler = document.querySelector("#sehirler");
+
+let newYorkCalisiyor = null;
+let zuricCalisiyor = null;
+let tokyoCalisiyor = null;
---
+++ function.js
@@ -0,0 +1,81 @@
+//Tokyo saati....
+function tokyoSaatiGoster() {
+  let dateTime = new Date().toLocaleString("en-UK", { timeZone: "Asia/Tokyo" });
+  dateTime = new Date(dateTime);
+  saat = dateTime.getHours();
+  dakika = dateTime.getMinutes();
+  saniye = dateTime.getSeconds();
+  sifirEkle();
+  let tokyoSaati = (document.getElementById("digital").innerHTML =
+    saat + ":" + dakika + ":" + saniye);
+
+  return tokyoSaati;
+}
+
+// new york saati....
+function newYorkSaatiGoster() {
+  let dateTime = new Date().toLocaleString("en-UK", {
+    timeZone: "america/new_york",
+  });
+  dateTime = new Date(dateTime);
+  saat = dateTime.getHours();
+  dakika = dateTime.getMinutes();
+  saniye = dateTime.getSeconds();
+  sifirEkle();
+  let neWSaatiGoster = (document.getElementById("digital").innerHTML =
+    saat + ":" + dakika + ":" + saniye);
+
+  return neWSaatiGoster;
+}
+
+//zürich saati....
+function zurichSaatiGoster() {
+  let dateTime = new Date().toLocaleString("en-UK", {
+    timeZone: "Europe/Vienna",
+  });
+  dateTime = new Date(dateTime);
+  saat = dateTime.getHours();
+  dakika = dateTime.getMinutes();
+  saniye = dateTime.getSeconds();
+  sifirEkle();
+  let zurichSaatiGoster = (document.getElementById("digital").innerHTML =
+    saat + ":" + dakika + ":" + saniye);
+
+  return zurichSaatiGoster;
+}
+
+// ana fonksiyon; saatleri çalıştırma fonksiyonu...
+sehirler.addEventListener("click", function (pEvent) {
+  saatiDurdur();
+
+  if (pEvent.target.id == "zurich") {
+    zuricCalisiyor = setInterval(zurichSaatiGoster, 1000);
+    baslik.innerHTML = "Zurich Saati";
+  } else if (pEvent.target.id == "newYork") {
+    newYorkCalisiyor = setInterval(newYorkSaatiGoster, 1000);
+    baslik.innerHTML = "New York Saati";
+  } else if (pEvent.target.id == "tokyo") {
+    tokyoCalisiyor = setInterval(tokyoSaatiGoster, 1000);
+    baslik.innerHTML = "Tokyo Saati";
+  }
+});
+
+//saatleri durdurma fonksiyonu
+function saatiDurdur() {
+  clearInterval(tokyoCalisiyor);
+  clearInterval(newYorkCalisiyor);
+  clearInterval(zuricCalisiyor);
+}
+
+// 10 dan küçük dakika ve saniyelere 0 sıfır ekleme...
+function sifirEkle() {
+  if (saniye < 10) {
+    saniye = "0" + saniye;
+  }
+  if (dakika < 10) {
+    dakika = "0" + dakika;
+  }
+  if (saat < 10) {
+    saat = "0" + saat;
+  }
+}
